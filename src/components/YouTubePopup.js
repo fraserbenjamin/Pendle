c89cef69768d1fa50119a1d9122c270b71b9d346
Fraser Benjamin <fraser.benjamin@outlook.com>
2020-09-04 20:16:33 +0100

    Added window size hook

--- src/components/YouTubePopup.js
+++ src/components/YouTubePopup.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import tw, {styled} from 'twin.macro';
 
+import useWindowSize from '../hooks/useWindowSize';
+
 const Container = tw.div`fixed w-full h-screen flex items-center justify-center top-0 left-0`;
 const Frame = styled.div`
     ${tw`w-11/12 shadow-lg bg-black z-50 left-0 mx-auto relative`}
@@ -9,22 +11,23 @@ const Player = tw.iframe`w-full h-full absolute top-0 left-0 border-none`;
 const Cover = tw.div`absolute w-full h-full bg-black opacity-75 top-0 left-0`;
 
 export default ({id, close, autoplay=true}) => {
+    const windowSize = useWindowSize();
     const [size, setSize] = useState([0,0]);
 
     useEffect(() => {
         const videoRatio = 16/9;
-        let screenRatio = window.innerWidth / window.innerHeight;
+        let screenRatio = windowSize.width / windowSize.height;
         let fraction = 0.9;
 
         if(screenRatio < videoRatio) {
             //Tall
-            setSize([window.innerWidth*fraction, window.innerWidth/videoRatio])
+            setSize([windowSize.width*fraction, windowSize.width/videoRatio])
         } else {
             //Wide
-            setSize([window.innerHeight*videoRatio, window.innerHeight*fraction])
+            setSize([windowSize.height*videoRatio, windowSize.height*fraction])
         }
 
-    }, []);
+    }, [windowSize]);
 
     return (
         <>
