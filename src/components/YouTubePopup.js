10a18a2f4a1c0214f28201a6af7879a7993a90b5
Fraser Benjamin <fraser.benjamin@outlook.com>
2020-09-04 20:19:50 +0100

    Simplified YouTube popup

--- src/components/YouTubePopup.js
+++ src/components/YouTubePopup.js
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import tw, {styled} from 'twin.macro';
+import tw from 'twin.macro';
 
 import useWindowSize from '../hooks/useWindowSize';
 
 const Container = tw.div`fixed w-full h-screen flex items-center justify-center top-0 left-0`;
-const Frame = styled.div`
-    ${tw`w-11/12 shadow-lg bg-black z-50 left-0 mx-auto relative`}
-`;
-const Player = tw.iframe`w-full h-full absolute top-0 left-0 border-none`;
+const Player = tw.iframe`shadow-lg bg-black z-50`;
 const Cover = tw.div`absolute w-full h-full bg-black opacity-75 top-0 left-0`;
 
 export default ({id, close, autoplay=true}) => {
@@ -33,9 +30,7 @@ export default ({id, close, autoplay=true}) => {
         <>
             <Cover />
             <Container onClick={close}>
-                <Frame style={{width: size[0], height: size[1]}}>
-                    <Player src={`https://www.youtube.com/embed/${id}?rel=0&autoplay=${autoplay ? "1" : "0"}&controls=1&fs=1`} allow="accelerometer; autoplay; modestbranding; encrypted-media; gyroscope; picture-in-picture" frameBorder="0" allowFullScreen></Player>
-                </Frame>
+                <Player width={size[0]} height={size[1]} src={`https://www.youtube.com/embed/${id}?rel=0&autoplay=${autoplay ? "1" : "0"}&controls=1&fs=1`} allow="accelerometer; autoplay; modestbranding; encrypted-media; gyroscope; picture-in-picture" frameBorder="0" allowFullScreen></Player>
             </Container>
         </>
   );
