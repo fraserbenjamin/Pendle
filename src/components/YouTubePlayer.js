efcb4a6ef5be3f6ade4fe92c016e6ce151859cc3
Fraser Benjamin <fraser.benjamin@outlook.com>
2020-09-25 19:23:50 +0100

    Fixed player loop

--- src/components/YouTubePlayer.js
+++ src/components/YouTubePlayer.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import tw from 'twin.macro';
 
-import useDimensions from "../hooks/useDimensions";
+const Player = tw.iframe`bg-black z-50`;
 
-const Player = tw.iframe`shadow-lg bg-black z-50`;
-
-export default ({id, autoplay=true}) => {
-    const videoRatio = 16/9;
-    const [ref, {width}] = useDimensions();
+export default ({id}) => {
 
     return (
-        <Player
-            ref={ref}
-            width="100%"
-            height={(width) ? width/videoRatio : 0}
-            src={`https://www.youtube.com/embed/${id}?rel=0&autoplay=${autoplay ? "1" : "0"}&controls=1&fs=1`}
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            frameBorder="0"
-            allowFullScreen/>
+        <div tw="w-full relative" style={{paddingBottom: "56.25%"}}>
+            <Player
+                tw="absolute"
+                width="100%"
+                height="100%"
+                src={`https://www.youtube.com/embed/${id}?rel=0&autoplay=0&controls=1&fs=1`}
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                frameBorder="0"
+                allowFullScreen/>
+        </div>
+
   );
 }
