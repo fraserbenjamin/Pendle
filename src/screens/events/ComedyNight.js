c33cb5773a65d7a1dc392bbf99b89dc41d47b3fa
Fraser Benjamin <fraser.benjamin@outlook.com>
2020-09-29 21:48:41 +0100

    Remove unused hook

--- src/screens/events/ComedyNight.js
+++ src/screens/events/ComedyNight.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState, useEffect} from 'react';
+import React, {useContext, useState} from 'react';
 import tw from 'twin.macro';
 import {useLocation} from "react-router-dom";
 import moment from "moment";
